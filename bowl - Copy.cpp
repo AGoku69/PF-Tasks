68a27bf666352bf93a8d7beb95deb604a1c1be4b
Abdullah Huda <abdullahhua2006@gmail.com>
2026-02-28 21:51:24 -0800

    ...

---
+++ bowl - Copy.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 0F");
+cout << "         *   *   *			"<<endl;
+cout << "          *   *   *			"<<endl;
+cout << "           *   *   *			"<<endl;
+cout << "          *   *   *			"<<endl;
+cout << "           *   *   *			"<<endl;
+cout << "            *   *   * 			"<<endl;
+cout << " ******************************        "<<endl;
+cout << "  \\                          /	"<<endl;
+cout << "   \\                        /         "<<endl;
+cout << "    \\                      /          "<<endl;
+cout << "     \\                    /		"<<endl;
+cout << "      *********************	        ";
+}
---
+++ car2 - Copy.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color CF");
+cout <<"                ________                   "<<endl;
+cout <<"               //  ||\\ \\\\                  "<<endl;
+cout <<"      ________//___||_\\ \\\\________          "<<endl;
+cout <<"     )  _                     _    \\         "<<endl;
+cout <<"     |_/ \\___________________/ \\____|      "<<endl;
+cout <<"    ___\\_/___________________\\_/______     ";
+}
